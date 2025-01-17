11bdafb470571be7fa3adf8c6b411f2716c168a1
liuzl.fnst <liuzl.fnst@fujitsu.com>
2025-01-17 11:53:06 +0800

    修改jest和yml文件

--- services/middlewares/user-middleware.js
+++ services/middlewares/user-middleware.js
@@ -2,6 +2,10 @@ function getUser(req, res, next) {
   return res.status(200).send();
 }
 
+function getUser2(req, res, next) {
+  return res.status(200).send();
+}
+
 module.exports = {
   getUser,
 };
