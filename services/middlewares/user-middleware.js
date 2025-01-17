6ea50e7176428176ae59fcf32f7088a0129b87df
liuzl.fnst <liuzl.fnst@fujitsu.com>
2025-01-17 11:54:42 +0800

    取消多余的测试部分

--- services/middlewares/user-middleware.js
+++ services/middlewares/user-middleware.js
@@ -2,10 +2,6 @@ function getUser(req, res, next) {
   return res.status(200).send();
 }
 
-function getUser2(req, res, next) {
-  return res.status(200).send();
-}
-
 module.exports = {
   getUser,
 };
