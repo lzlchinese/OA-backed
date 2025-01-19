dab564100d3db847c7f0a6e37005b8e707a9b375
liuzhonglei <1761997923@qq.com>
2025-01-19 23:35:41 +0800

    format coding

--- services/middlewares/user-middleware.js
+++ services/middlewares/user-middleware.js
@@ -1,8 +1,8 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 async function getUser(req, res, next) {
-  const users = await User.find();  // 返回所有文档
-  return  res.json(users);
+  const users = await User.find(); // 返回所有文档
+  return res.json(users);
 }
 
 module.exports = {
