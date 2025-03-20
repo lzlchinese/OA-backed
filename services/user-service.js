b10168e40f1bb45a668914d4f0f3fcae4cd2b9dd
liuzhonglei <1761997923@qq.com>
2025-03-20 21:07:50 +0800

    生成和获取个人信息的接口上传

--- services/user-service.js
+++ services/user-service.js
@@ -1,7 +1,9 @@
 const UserMiddleWare = require("./middlewares/user-middleware");
 
 const getUser = [UserMiddleWare.getUser];
+const createUser = [UserMiddleWare.createUser];
 
 module.exports = {
   getUser,
+  createUser,
 };
